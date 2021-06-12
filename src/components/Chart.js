525d34b95f6174a7ff1475bec233fb86843a75de
OBEJG <79863101+OBEJG@users.noreply.github.com>
2021-06-12 08:39:30 +0200

    Update Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,15 +1,51 @@
-import React, {Component} from 'react'
-import {Line} from 'react-chartjs-2'
+import React from 'react'
+import { Line } from 'react-chartjs-2'
 
-class Chart extends Component{
+const Chart = () => {
 
-	render() {
-		return (
-			<div className="chart">
-
-			</div>
-		)
-	}
+  return (
+    <div>
+      <Line
+        data={{
+          labels: ['5 godzin temu', '4 godziny temu', '3 godziny temu', '2 godziny temu', 'godzina temu', '5 godzin temu', '4 godziny temu', '3 godziny temu', '2 godziny temu', 'godzina temu', ],
+          datasets: [
+            {
+              label: 'Ilość opadów',
+              data: [0.4, 0, 0.2, 0.5, 1.2, 0.4, 0, 0.2, 0.5, 1.2, ],
+              backgroundColor: [
+                'rgba(255, 255, 255, 1)',
+              ],
+              borderColor: [
+                'rgba(255, 255, 255, 1)',
+              ],
+              borderWidth: 5,
+			  tension: 0.35
+            },
+          ],
+        }}
+        height={400}
+        width={300}
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          legend: {
+            labels: {
+              fontSize: 25,
+            },
+          },
+		  pointRadius: 0
+        }}
+      />
+    </div>
+  )
 }
 
 export default Chart
